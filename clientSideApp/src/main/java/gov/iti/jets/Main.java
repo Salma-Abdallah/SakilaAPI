231c618002435a5ceefb4fa4416eece15af0279a
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-03-28 23:12:54 +0200

    add

---
+++ clientSideApp/src/main/java/gov/iti/jets/Main.java
@@ -0,0 +1,14 @@
+package gov.iti.jets;
+
+
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+    
+
+        
+    }
+
+}
---
+++ serverSideApp/src/main/java/gov/iti/jets/App.java
@@ -0,0 +1,11 @@
+package gov.iti.jets;
+
+
+
+public class App {
+    public static void main( String[] args )
+    {
+    
+    }
+    
+}
