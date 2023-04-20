4298dc2225bac534d85ec1694f4e409bfa2ca185
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-04-20 21:19:14 +0200

    Update Rental.java

--- RestApi/src/main/java/gov/iti/jets/web/restServices/Rental.java
+++ RestApi/src/main/java/gov/iti/jets/web/restServices/Rental.java
@@ -5,9 +5,12 @@ import gov.iti.jets.web.dto.RentalDto;
 import gov.iti.jets.web.dto.rental.RentalInventoryDto;
 import gov.iti.jets.web.dto.rental.RentalStaffDto;
 import gov.iti.jets.web.services.RentalService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
 
 import java.util.List;
 
+@Path("rental")
 public class Rental {
     private RentalService rentalService;
 
@@ -15,31 +18,64 @@ public class Rental {
         rentalService = new RentalService();
     }
 
-    public List<RentalDto> getAllRentals() {
-        return rentalService.getAllRentals();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllRentals(@Context UriInfo uriInfo) {
+        List<RentalDto> rentalDtos = rentalService.getAllRentals();
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(rentalDtos).links(link).build();
     }
 
-    public RentalDto getRentalById(int id) {
-        return rentalService.getRentalById(id);
+    @GET
+    @Path("{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRentalById(@PathParam("id") int id , @Context UriInfo uriInfo) {
+        RentalDto rentalDto = rentalService.getRentalById(id);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(rentalDto).links(link).build();
     }
 
+    @POST
+    @Consumes({"application/xml", "application/json"})
     public void addRental(RentalDto rentalDto) {
         rentalService.addRental(rentalDto);
     }
 
+    @PUT
+    @Consumes({"application/xml", "application/json"})
     public void updateRental(RentalDto rentalDto) {
         rentalService.updateRental(rentalDto);
     }
 
-    public RentalInventoryDto getInventoryByRental(int id) {
-        return rentalService.getInventoryByRental(id);
+    @GET
+    @Path("inventory/{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getInventoryByRental(@PathParam("id") int id , @Context UriInfo uriInfo) {
+        RentalInventoryDto rentalInventoryDto = rentalService.getInventoryByRental(id);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(rentalInventoryDto).links(link).build();
     }
 
-    public RentalStaffDto getStaffByRental (int id) {
-        return rentalService.getStaffByRental(id) ;
+    @GET
+    @Path("staff/{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getStaffByRental (@PathParam("id") int id , @Context UriInfo uriInfo) {
+        RentalStaffDto rentalStaffDto = rentalService.getStaffByRental(id) ;
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(rentalStaffDto).links(link).build();
     }
 
-    public CustomerDto getCustomerByRental(int id) {
-        return rentalService.getCustomerByRental(id);
+    @GET
+    @Path("customer/{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCustomerByRental(@PathParam("id") int id , @Context UriInfo uriInfo) {
+        CustomerDto customerDto = rentalService.getCustomerByRental(id);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(customerDto).links(link).build();
     }
 }
