7ef692a18b1d3715257a22bb90cf5ebf6957e408
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-04-20 21:29:08 +0200

    Update Staff.java

--- RestApi/src/main/java/gov/iti/jets/web/restServices/Staff.java
+++ RestApi/src/main/java/gov/iti/jets/web/restServices/Staff.java
@@ -3,9 +3,12 @@ package gov.iti.jets.web.restServices;
 import gov.iti.jets.web.dto.AddressDto;
 import gov.iti.jets.web.dto.StaffDto;
 import gov.iti.jets.web.services.StaffService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
 
 import java.util.List;
 
+@Path("staff")
 public class Staff {
     private StaffService staffService;
 
@@ -13,35 +16,74 @@ public class Staff {
         staffService = new StaffService();
     }
 
-    public List<StaffDto> getAllStaff() {
-        return staffService.getAllStaff();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllStaff(@Context UriInfo uriInfo) {
+        List<StaffDto> staffDtos = staffService.getAllStaff();
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(staffDtos).links(link).build();
     }
 
-    public StaffDto getStaffById(int id) {
-        return staffService.getStaffById(id);
+    @GET
+    @Path("{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getStaffById(@PathParam("id") int id , @Context UriInfo uriInfo) {
+        StaffDto staffDto = staffService.getStaffById(id);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(staffDto).links(link).build();
     }
 
+    @POST
+    @Consumes({"application/xml", "application/json"})
     public void addStaff(StaffDto staffDto) {
         staffService.addStaff(staffDto);
     }
 
+    @PUT
+    @Consumes({"application/xml", "application/json"})
     public void updateStaff(StaffDto staffDto) {
         staffService.updateStaff(staffDto);
     }
 
-    public List<StaffDto> getStaffByName(String name) {
-        return staffService.getStaffByName(name);
+    @GET
+    @Path("{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getStaffByName(@PathParam("name") String name , @Context UriInfo uriInfo) {
+        List<StaffDto> staffDtos = staffService.getStaffByName(name);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(staffDtos).links(link).build();
     }
 
-    public AddressDto getStaffAddress(int id) {
-        return staffService.getStaffAddress(id);
+    @GET
+    @Path("address/{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getStaffAddress(@PathParam("id") int id , @Context UriInfo uriInfo) {
+        AddressDto addressDto = staffService.getStaffAddress(id);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(addressDto).links(link).build();
     }
 
-    public long getNumberActiveStaff() {
-        return staffService.getNumberActiveStaff();
+    @GET
+    @Path("active")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getNumberActiveStaff( @Context UriInfo uriInfo) {
+        long count = staffService.getNumberActiveStaff();
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(count).links(link).build();
     }
 
-    public long getNumberInActiveStaff() {
-        return staffService.getNumberInActiveStaff();
+    @GET
+    @Path("inActive")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getNumberInActiveStaff( @Context UriInfo uriInfo) {
+        long count = staffService.getNumberInActiveStaff();
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(count).links(link).build();
     }
 }
