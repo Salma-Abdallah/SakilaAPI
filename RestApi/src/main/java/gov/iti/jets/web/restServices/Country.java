986501206960e83e7086d421890db4cc825dd780
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-04-20 19:45:05 +0200

    Update Country.java

--- RestApi/src/main/java/gov/iti/jets/web/restServices/Country.java
+++ RestApi/src/main/java/gov/iti/jets/web/restServices/Country.java
@@ -2,11 +2,13 @@ package gov.iti.jets.web.restServices;
 
 import gov.iti.jets.web.dto.CountryDto;
 import gov.iti.jets.web.services.CountryService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
 
 
 import java.util.List;
 
-
+@Path("country")
 public class Country {
     private CountryService countryService;
 
@@ -14,23 +16,44 @@ public class Country {
         countryService = new CountryService();
     }
 
-    public List<CountryDto> getAllCountries() {
-        return countryService.getAllCountries();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllCountries(@Context UriInfo uriInfo) {
+        List<CountryDto> countryDtos = countryService.getAllCountries();
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(countryDtos).links(link).build();
     }
 
-    public CountryDto getCountryById(int id) {
-        return countryService.getCountryById(id);
+    @GET
+    @Path("{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCountryById(@PathParam("id") int id , @Context UriInfo uriInfo) {
+        CountryDto countryDto = countryService.getCountryById(id);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(countryDto).links(link).build();
     }
 
+    @POST
+    @Consumes({"application/xml", "application/json"})
     public void addCountry(CountryDto countryDto) {
         countryService.addCountry(countryDto);
     }
 
+    @PUT
+    @Consumes({"application/xml", "application/json"})
     public void updateCountry(CountryDto countryDto) {
         countryService.updateCountry(countryDto);
     }
 
-    public CountryDto getCountryByName(String name) {
-        return countryService.getCountryByName(name);
+    @GET
+    @Path("{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCountryByName(@PathParam("name") String name ,@Context UriInfo uriInfo) {
+        CountryDto countryDto = countryService.getCountryByName(name);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(countryDto).links(link).build();
     }
 }
