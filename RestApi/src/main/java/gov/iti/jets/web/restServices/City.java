4fc60c42209cfaaf9d5d2ca35d8eda863ae5a452
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-04-20 19:39:45 +0200

    Update City.java

--- RestApi/src/main/java/gov/iti/jets/web/restServices/City.java
+++ RestApi/src/main/java/gov/iti/jets/web/restServices/City.java
@@ -2,10 +2,12 @@ package gov.iti.jets.web.restServices;
 
 import gov.iti.jets.web.dto.CityDto;
 import gov.iti.jets.web.services.CityService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
 
 import java.util.List;
 
-
+@Path("city")
 public class City {
     private CityService cityService;
 
@@ -13,23 +15,44 @@ public class City {
         cityService = new CityService();
     }
 
-    public List<CityDto> getAllCities() {
-        return cityService.getAllCities();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllCities(@Context UriInfo uriInfo) {
+        List<CityDto> cityDtos = cityService.getAllCities();
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(cityDtos).links(link).build();
     }
 
-    public CityDto getCityById(int id) {
-        return cityService.getCityById(id);
+    @GET
+    @Path("{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCityById(@PathParam("id") int id , @Context UriInfo uriInfo) {
+        CityDto cityDto = cityService.getCityById(id);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(cityDto).links(link).build();
     }
 
+    @POST
+    @Consumes({"application/xml", "application/json"})
     public void addCity(CityDto cityDto) {
         cityService.addCity(cityDto);
     }
 
+    @PUT
+    @Consumes({"application/xml", "application/json"})
     public void updateCity(CityDto cityDto) {
         cityService.updateCity(cityDto);
     }
 
-    public CityDto getCityByName(String name) {
-        return cityService.getCityByName(name);
+    @GET
+    @Path("{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCityByName(@PathParam("name") String name ,@Context UriInfo uriInfo) {
+        CityDto cityDto = cityService.getCityByName(name);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(cityDto).links(link).build();
     }
 }
