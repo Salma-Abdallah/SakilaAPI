7b49d36b3ee4d22e0026df2b0d87a2f47d8f339e
Salma-Abdallah <119084967+Salma-Abdallah@users.noreply.github.com>
2023-04-20 21:31:56 +0200

    Update Store.java

--- RestApi/src/main/java/gov/iti/jets/web/restServices/Store.java
+++ RestApi/src/main/java/gov/iti/jets/web/restServices/Store.java
@@ -4,10 +4,12 @@ package gov.iti.jets.web.restServices;
 import gov.iti.jets.web.dto.StoreDto;
 import gov.iti.jets.web.dto.StoreUpdateDto;
 import gov.iti.jets.web.services.StoreService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
 
 import java.util.List;
 
-
+@Path("store")
 public class Store {
     private StoreService storeService;
 
@@ -15,14 +17,27 @@ public class Store {
         storeService = new StoreService();
     }
 
-    public List<StoreDto> getAllStores() {
-        return storeService.getAllStores();
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllStores(@Context UriInfo uriInfo) {
+        List<StoreDto> storeDtos = storeService.getAllStores();
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(storeDtos).links(link).build();
     }
 
-    public StoreDto getStoreById(int id) {
-        return storeService.getStoreById(id);
+    @GET
+    @Path("{id: [0-9]+}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getStoreById(@PathParam("id") int id , @Context UriInfo uriInfo) {
+        StoreDto storeDto = storeService.getStoreById(id);
+        Link link = Link.fromUriBuilder(uriInfo.getAbsolutePathBuilder()).rel("self").build();
+
+        return Response.ok(storeDto).links(link).build();
     }
 
+    @POST
+    @Consumes({"application/xml", "application/json"})
     public void addStore(StoreUpdateDto storeDto) {
         storeService.addStore(storeDto);
     }
